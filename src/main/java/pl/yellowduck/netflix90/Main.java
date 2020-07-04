2304dcc6f7859cfc264baee72e43b868eb6f1ad2
Jacek D <j_d@o2.pl>
2020-07-04 12:42:44 +0200

    Videocassete "The Room" created :-)

--- src/main/java/pl/yellowduck/netflix90/Main.java
+++ src/main/java/pl/yellowduck/netflix90/Main.java
@@ -1,16 +1,53 @@
 package pl.yellowduck.netflix90;
 
-import pl.yellowduck.resources.Person;
-import pl.yellowduck.resources.Actor;
-import pl.yellowduck.resources.Director;
-import pl.yellowduck.resources.Sex;
+import pl.yellowduck.resources.*;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-
 public class Main {
+    public static void main(String[] args) {
+        Director director = new Director("Tommy", "Wiseau", Sex.MALE);
+        Actor tommyActor = new Actor ("Tommy", "Wiseau", Sex.MALE);
+        Actor julietteActor = new Actor ("Juliette", "Danielle", Sex.FEMALE);
+        Actor gregActor = new Actor ("Greg", "Sastero", Sex.MALE);
+        /*
+        List<Person> persons = new ArrayList<>();
 
+        persons.add(director);
+        persons.add(tommyActor);
+        persons.add(julietteActor);
+        persons.add(gregActor);
+        */
+        Set<Actor> actors = new HashSet<Actor>();
+
+        actors.add(tommyActor);
+        actors.add(julietteActor);
+        actors.add(gregActor);
+
+        /*
+        for (Person person : persons) {
+            person.introduce();
+        }
+
+        System.out.println("Hello Netflix90");
+        */
+
+        VideoCasette theRoom = new VideoCasette(
+                "12345",
+                BigDecimal.valueOf(100),
+                "The Room",
+                Category.DRAMA,
+                actors,
+                director
+        );
+    }
+}
+/*
+public class Main {
   public static void main(String[] args) {
     Director director = new Director("Tommy", "Wiseau", Sex.MALE);
     Actor tommyActor = new Actor ("Tommy", "Wiseau", Sex.MALE);
@@ -30,3 +67,4 @@ public class Main {
     System.out.println("Hello Netflix90");
   }
 }
+*/
--- src/main/java/pl/yellowduck/resources/VideoCasette.java
+++ src/main/java/pl/yellowduck/resources/VideoCasette.java
@@ -10,4 +10,13 @@ public class VideoCasette {
     private Category category;
     private Set<Actor> actors;
     private Director director;
+
+    public VideoCasette(String id, BigDecimal price, String title, Category category, Set<Actor> actors, Director director) {
+        this.id = id;
+        this.price = price;
+        this.title = title;
+        this.category = category;
+        this.actors = actors;
+        this.director = director;
+    }
 }
